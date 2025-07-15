0f1d81ce0593e391d230063eccaceb3bd6bb44b0
sasiiii02 <sasikaru15@gmail.com>
2025-07-15 12:46:02 +0530

    All the sections creates

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,33 @@
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+const App=()=>(
+  <main className="relative">
+    Nav 
+    <section className="xl:padding-l wide:padding-r padding-b">
+      Hero 
+    </section>
+    <section className="padding">
+      popular products
+    </section>
+     <section className="padding">
+      super quality
+    </section>
+     <section className="padding-x py-10">
+      services
+    </section>
+     <section className="padding">
+      special offer
+    </section>
+    <section className="bg-pale-blue padding">
+      customer reviews
+    </section>
+    <section className="padding-x sm:py-32 py-16 w-full">
+      subscribe
+    </section>
+    <section className="bg-black padding-x padding-t pb-8">
+      footer
+    </section>
+
+
+    
+  </main>
+);
+export default App;
