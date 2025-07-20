07b7bffdae59341a37be0f4285baa80d75757fb6
sasiiii02 <sasikaru15@gmail.com>
2025-07-20 23:09:45 +0530

    Button updated

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -4,8 +4,8 @@ const Button = ({label,iconURL}) => {
   return (
     <button className='border-2 flex justify-center items-center gap-2 px-7 py-4 font-montserrat text-m leading-none bg-coral-red rounded-full text-white'>
         {label}
-        <img src={iconURL} alt='arrow right icon' className='ml-2 rounded-full w-5 h-5'/>
-        
+    {iconURL && <img src={iconURL} alt='arrow right icon' className='ml-2 rounded-full w-5 h-5'/>
+}        
     </button>
   )
 }
